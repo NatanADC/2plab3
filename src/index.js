108c12f57c6c78e82eb52d5f43ac63e0ce3c5e3f
NatanDC <natandc740@gmail.com>
2019-03-14 07:06:04 -0600

    final

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const List = props => (
+  <ul>
+    {
+      props.items.map((item, index) => <li key={index}>{item} </li> )
+    }
+  </ul>
+);
+
+class Twitter extends React.Component{
+    constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <form  onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
+
+}
+
+
+ReactDOM.render(<Twitter />, document.getElementById('root'));
+
+
+//<h2>It is {new Date().toLocaleTimeString()}.</h2>
+  //      <h2>It is {new Date().toLocaleDateString()}.</h2>  
